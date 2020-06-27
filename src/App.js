07552cda4dd54a95a368988a7fbcb0c3ad1ba97d
JulianCode <hijuliansosa@protonmail.com>
2020-06-27 17:21:32 -0500

    ui: root component (App), base styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
-import styled from 'styled-components';
+import React from 'react'
+import gradiIsotipo from './images/design/misc/logo-s4n.svg'
+import styled from 'styled-components'
 
-const Heading = styled.h1`
-  background-color: ${props => props.theme.primaryColor};
-  color: ${props => props.theme.lightColor}
-`;
+const Alerts = styled.div`
+  display: none;
+`
+const Wraper = styled.div`
+  height: 100%;
+  position: relative;
+`
+const Brand = styled.img`
+  object-fit: content;
+  position: absolute;
+  right: 40px;
+  top: 22px;
+  width: 64px;
+`
+const App = () => (
+  <Wraper>
+    <Brand src={gradiIsotipo} />
+    <Alerts/>
+  </Wraper>
+)
 
-function App() {
-  return (
-    <div className="App">
-      <Heading>Hey!</Heading>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 // Styles
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { normalize, Normalize } from "styled-normalize";
-import * as Theme from "./config/theme/";
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize as Normalize } from "styled-normalize"
+import * as Theme from "./config/theme/"
 
 export const GlobalStyle = createGlobalStyle`
   ${Normalize}
+  /* Global Styles */
+  html, body, #root { height: 100%; }
+  h1, h2, h3, h4, h5, h6 { margin-top: 0;line-height: 1em; }
+  p { margin-top: 0;line-height: 1.2em; }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    background-color: ${props => props.theme.lightColor};
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
 `;
 
 ReactDOM.render(
@@ -23,4 +38,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
