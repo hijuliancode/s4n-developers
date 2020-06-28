a97189f89b75cfd01cf466df222978c5170c5198
JulianCode <hijuliansosa@protonmail.com>
2020-06-27 20:56:38 -0500

    Feature: Add Routing, Update Components

---
+++ src/components/Home/FormRegister/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const FormRegisterElm = styled.div``
+
+const FormRegisterComponent = () => (
+  <FormRegisterElm></FormRegisterElm>
+)
+ 
+export default FormRegisterComponent;
